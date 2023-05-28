c52a293931ff964bab79409dea3e0f525070c33d
albano <aeg2k@hotmail.com>
2023-05-27 23:55:48 -0300

    Agregado busqueda de películas

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-let pagina = 1;
+let page = 1;
 const btnAnterior = document.getElementById('btnAnterior');
 const btnSiguiente = document.getElementById('btnSiguiente');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 const filter = document.getElementById('filter-opt');
-const button = document.querySelector('.button')
-const nav = document.querySelector('.nav')
+const button = document.querySelector('.button');
+const nav = document.querySelector('.nav');
 
 button.addEventListener('click',()=>{
-    nav.classList.toggle('activo')
+    nav.classList.toggle('activo');
 })
 
 filter.addEventListener("change", function() {
-	pagina = 1;
-	cargarPeliculas(filter.value);
+	page = 1;
+	getMovies(filter.value);
 });
 
 btnSiguiente.addEventListener('click', () => {
-	if(pagina < 1000){
-		pagina += 1;
-		cargarPeliculas(filter.value);
+	if(page < 1000){
+		page += 1;
+		getMovies(filter.value);
 	}
 });
 
 btnAnterior.addEventListener('click', () => {
-	if(pagina > 1){
-		pagina -= 1;
-		cargarPeliculas(filter.value);
+	if(page > 1){
+		page -= 1;
+		getMovies(filter.value);
 	}
 });
 
@@ -34,14 +34,15 @@ close.addEventListener('click', () => {
   modal_container.classList.remove('show');
 });
 
-function btnClose () {
-	modal_container.classList.remove('show');
-}
+
+btnClose = () => modal_container.classList.remove('show');
 
 const getMovie = async(id) => {  
 	try {
 	  	const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=3fed0b6eb79f693a90195039eaca3561&language=es-MX`)
-	  	if(response.status === 200){
+	  	
+    	// Si la respuesta es correcta
+		if(response.status === 200){
 			const movie = await response.json();
 			
 			let movieDetails = '';
@@ -59,43 +60,46 @@ const getMovie = async(id) => {
 		modal_container.classList.add('show');
 		document.getElementById('modal_container').innerHTML = movieDetails;
 
+		} else if(response.status === 401){
+			console.log('Error de autenticación');
+		} else if(response.status === 404){
+			console.log('Recurso inexistente');
 		} else {
 			console.log('Ha ocurrido un error');
 		}
+
 	} catch(error){
 		console.log(error);
 	}
 }
 
-const cargarPeliculas = async(filter) => {
+const getMovies = async(filter) => {
 	let optSelected = filter;
 	try {
-		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/${optSelected}?api_key=3fed0b6eb79f693a90195039eaca3561&language=es-MX&page=${pagina}`);
+		const response = await fetch(`https://api.themoviedb.org/3/movie/${optSelected}?api_key=3fed0b6eb79f693a90195039eaca3561&language=es-MX&page=${page}`);
 	
-		console.log(respuesta);
-
 		// Si la respuesta es correcta
-		if(respuesta.status === 200){
-			const datos = await respuesta.json();
+		if(response.status === 200){
+			const datos = await response.json();
 			
-			let peliculas = '';
-			datos.results.forEach(pelicula => {
-				peliculas += `
+			let movies = '';
+			datos.results.forEach(movie => {
+				movies += `
 					<div class="pelicula">
-						<img class="card" onclick="getMovie(${pelicula.id})" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-						<h6 class="textos">${pelicula.title}</h6>
+						<img class="card" onclick="getMovie(${movie.id})" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+						<h6 class="textos">${movie.title}</h6>
 					</div>
 				`;
 			});
 
-			document.getElementById('contenedor').innerHTML = peliculas;
+			document.getElementById('contenedor').innerHTML = movies;
 
-		} else if(respuesta.status === 401){
-			console.log('Pusiste la llave mal');
-		} else if(respuesta.status === 404){
-			console.log('La pelicula que buscas no existe');
+		} else if(response.status === 401){
+			console.log('Error de autenticación');
+		} else if(response.status === 404){
+			console.log('Recurso inexistente');
 		} else {
-			console.log('Hubo un error y no sabemos que paso');
+			console.log('Ha ocurrido un error');
 		}
 
 	} catch(error){
@@ -104,5 +108,5 @@ const cargarPeliculas = async(filter) => {
 
 }
 
-cargarPeliculas(filter.value);
+getMovies(filter.value);
 
